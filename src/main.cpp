c507cf10448f4ab122f31f7b3460e6c15885a135
DeFlesz <nieflesz@gmail.com>
2023-01-24 01:28:39 +0100

    hotfix, glfwInit()

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
 int main()
 {
     std::cout << "Hello, world!\n";
     GLFWwindow *window;
+
+    glfwInit();
 
     window = glfwCreateWindow(800, 600, "Okno", NULL, NULL);
     if (!window)
